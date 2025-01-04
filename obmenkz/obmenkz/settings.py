aaaac7270b40da38b721b5185c410e1256720402
Ramazan <ospanr4@gmail.com>
2025-01-04 16:20:36 +0500

    add psgres acces

--- obmenkz/obmenkz/settings.py
+++ obmenkz/obmenkz/settings.py
@@ -70,12 +70,25 @@ WSGI_APPLICATION = 'obmenkz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Database configuration
+if os.environ.get('POSTGRES_DB') and os.environ.get('POSTGRES_USER'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB'),
+            'USER': os.environ.get('POSTGRES_USER'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+            'PORT': os.environ.get('POSTGRES_PORT'),
+            'HOST': os.environ.get('POSTGRES_HOST'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
